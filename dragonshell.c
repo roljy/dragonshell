70ddf92379a30f30dae8b246266d6548d5535a50
roljy <62124462+roljy@users.noreply.github.com>
2024-09-16 13:57:50 -0600

    WIP basic prompt IO

--- dragonshell.c
+++ dragonshell.c
@@ -2,6 +2,14 @@
 // Tawfeeq Mannan
 
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// useful constants
+const size_t LINE_LENGTH = 100;  // # characters per input line
+const size_t MAX_ARGS = 5;  // # args per command
+const size_t MAX_LENGTH = 20;  // # characters per arg
+const size_t MAX_BG_PROC = 1; // # background processes
 
 
 /**
@@ -24,7 +32,31 @@ void tokenize(char *str, const char *delim, char **argv)
 }
 
 
+void display_prompt(char **argv)
+{
+    printf("dragonshell > ");
+
+    char *buffer;
+    buffer = (char *) malloc(LINE_LENGTH * sizeof(char));
+    getline(&buffer, (size_t*)&LINE_LENGTH, stdin);
+    tokenize(buffer, " ", argv);
+    free(buffer);
+}
+
+
 int main(int argc, char *argv[])
 {
+    // welcome message at shell start
+    printf("Welcome to Dragon Shell!\n\n");
+
+    char **args;
+    // TODO allocate array of strings correctly
+    args = (char **) malloc(MAX_ARGS * MAX_LENGTH * sizeof(char));
+    display_prompt(args);
+    printf("You typed: \n");
+    for (size_t i = 0; i < MAX_ARGS; i++)
+        printf("%s\n", args[i]);
+    free(args);
+
     return 0;
 }
