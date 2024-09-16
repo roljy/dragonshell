c68861048e72fab7bc7e149c63b44b560cd24cf8
roljy <62124462+roljy@users.noreply.github.com>
2024-09-16 13:04:40 -0600

    Add Makefile, reformat main C file and readme header

---
+++ Makefile
@@ -0,0 +1,17 @@
+CC = gcc
+CFLAGS = -Wall
+
+dragonshell: compile
+	$(CC) $(CFLAGS) -o dragonshell *.o
+
+# leverage make's implicit recipe for object files
+compile: dragonshell.o
+
+clean:
+	rm *.o dragonshell dsh_debug
+
+valgrind: dsh_debug
+	valgrind --tool=memcheck --leak-check=yes ./dsh_debug
+
+dsh_debug:
+	$(CC) $(CFLAGS) -O0 -g -o dsh_debug *.c
--- dragonshell.c
+++ dragonshell.c
@@ -1,22 +1,30 @@
-#include<string.h>
+// dragonshell.c
+// Tawfeeq Mannan
+
+#include <string.h>
+
 
 /**
- * @brief Tokenize a C string 
- * 
- * @param str - The C string to tokenize 
- * @param delim - The C string containing delimiter character(s) 
- * @param argv - A char* array that will contain the tokenized strings
- * Make sure that you allocate enough space for the array.
+ * @brief Tokenize a C string
+ *
+ * @param str C string to tokenize
+ * @param delim C string containing delimiter character(s)
+ * @param argv char* array that will contain the tokenized strings.
+ *             Make sure that you allocate enough space for the array.
  */
-void tokenize(char* str, const char* delim, char ** argv){
-  char* token;
-  token = strtok(str, delim);
-  for(size_t i = 0; token != NULL; ++i){
-    argv[i] = token;
-  token = strtok(NULL, delim);
-  }
+void tokenize(char *str, const char *delim, char **argv)
+{
+    char *token;
+    token = strtok(str, delim);
+    for (size_t i = 0; token != NULL; ++i)
+    {
+        argv[i] = token;
+        token = strtok(NULL, delim);
+    }
 }
 
-int main(int argc, char *argv[]){
-  return 0;
+
+int main(int argc, char *argv[])
+{
+    return 0;
 }
