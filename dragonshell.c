9bbcdab6f48ca08093cb095470fbad8bb0016808
roljy <62124462+roljy@users.noreply.github.com>
2024-09-17 15:08:00 -0600

    Handle cd, add preliminary exit condition

--- dragonshell.c
+++ dragonshell.c
@@ -1,15 +1,19 @@
 // dragonshell.c
 // Tawfeeq Mannan
 
-#include <string.h>     // strtok, strcspn
+#include <string.h>     // strtok, strcspn, strcmp
 #include <stdio.h>      // printf, fgets
-#include <stdlib.h>     // malloc, free
+#include <stdbool.h>    // true/false
+#include <unistd.h>     // chdir
 
 // useful constants
 const size_t LINE_LENGTH = 100;  // num of characters per input line
 const size_t MAX_ARGS = 5;  // num of args per command
 const size_t MAX_LENGTH = 20;  // num of characters per arg
 const size_t MAX_BG_PROC = 1; // num of background processes
+
+// global vars
+bool keep_shell_alive = true;
 
 
 /**
@@ -50,6 +54,17 @@ void display_prompt(char *buffer)
 
 
 /**
+ * @brief Log an error message
+ * 
+ * @param msg Explanation of the error that occurred
+ */
+void log_error(char *msg)
+{
+    printf("dragonshell: %s\n", msg);
+}
+
+
+/**
  * @brief Central master function to handle all requests,
  *        delegating to subroutines as necessary.
  * @param argc Number of input arguments (tokens)
@@ -57,9 +72,28 @@ void display_prompt(char *buffer)
  */
 void handle_request(int argc, char **argv)
 {
-    if (argc == 0)
+    if (argc == 0)  // empty line. no-op
         return;
-    printf("%s\n", argv[0]);
+
+    if (strcmp(argv[0], "cd") == 0)
+    {
+        if (argc < 2)
+        {
+            log_error("Expected argument to \"cd\"");
+            return;
+        }
+        // cd only needs argv[1], can ignore anything that follows
+        if (chdir(argv[1]) != 0)
+        {
+            log_error("No such file or directory");
+        }
+    }
+
+    else if (strcmp(argv[0], "exit") == 0)
+    {
+        // TODO gracefully exit
+        keep_shell_alive = false;
+    }
 }
 
 
@@ -76,16 +110,19 @@ int main(int argc, char *argv[])
     // display welcome message at start
     printf("Welcome to Dragon Shell!\n\n");
 
-    // get a command from the user
-    char buffer[LINE_LENGTH];
-    display_prompt(buffer);
+    while (keep_shell_alive)
+    {
+        // get a command from the user
+        char buffer[LINE_LENGTH];
+        display_prompt(buffer);
 
-    // split the user-provided command into tokens for parsing
-    char *args[MAX_ARGS * 3];  // x3 for safety, in case of multi-command
-    size_t args_cnt = tokenize(buffer, " ", args);
+        // split the user-provided command into tokens for parsing
+        char *args[MAX_ARGS * 3];  // x3 for safety, in case of multi-command
+        size_t args_cnt = tokenize(buffer, " ", args);
 
-    // handle the arguments accordingly
-    handle_request(args_cnt, args);
+        // handle the arguments accordingly
+        handle_request(args_cnt, args);
+    }
 
     return 0;
 }
